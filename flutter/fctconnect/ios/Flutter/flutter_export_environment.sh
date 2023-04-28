c066f8692ec6ccfae1f7172f1a127372a90eb5c1
jhgarciaxd <jh.garcia@campus.fct.unl.pt>
2023-04-28 14:58:33 +0100

    Correção das libs desaparecidas

--- flutter/fctconnect/ios/Flutter/flutter_export_environment.sh
+++ flutter/fctconnect/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/joaohenriquegarcia/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/joaohenriquegarcia/Library/CloudStorage/OneDrive-Personal/Documents/Universidade/3ANO/2SEMESTRE/ADC/PROJETO/fctconnect"
+export "FLUTTER_APPLICATION_PATH=/Users/joaohenriquegarcia/Library/CloudStorage/OneDrive-Personal/Documents/Universidade/3ANO/2SEMESTRE/ADC/PROJETO/adc-project/flutter/fctconnect"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
