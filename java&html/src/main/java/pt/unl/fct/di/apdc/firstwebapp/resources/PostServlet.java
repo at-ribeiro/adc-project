20eafb752393148c332ff2c9d30d9c0b28b4c8c3
at-ribeiro <at.ribeiro@campus.fct.unl.pt>
2023-05-12 17:13:45 +0100

    muitas mudancas

--- java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/PostServlet.java
+++ java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/PostServlet.java
@@ -7,15 +7,19 @@ import com.google.cloud.storage.*;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.storage.Blob;
 import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
-import main.java.pt.unl.fct.di.apdc.firstwebapp.util.PostData;
+import pt.unl.fct.di.apdc.firstwebapp.util.FeedData;
+import pt.unl.fct.di.apdc.firstwebapp.util.PostData;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 
@@ -99,12 +103,13 @@ public class PostServlet extends HttpServlet {
             if (imageStream != null) {
 
                 imageName = request.getPart("image").getSubmittedFileName();
-                BlobId blobId = BlobId.of(bucketName, imageName + username);
+                BlobId blobId = BlobId.of(bucketName,  username + "-" + imageName);
 
                 BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setAcl(Collections.singletonList(
                                                 Acl.newBuilder(Acl.User.ofAllUsers(), Acl.Role.READER).build())).build();
 
                 byte[] imageBytes = IOUtils.toByteArray(imageStream);
+
 
                 storage.create(blobInfo, imageBytes);
             }
@@ -112,9 +117,10 @@ public class PostServlet extends HttpServlet {
             Key postKey = datastore.newKeyFactory()
                                 .setKind("Post")
                                 .addAncestor(PathElement.of("User", username))
-                                .newKey(username + "-" + timestamp);
+                                .newKey(username + "-" + System.currentTimeMillis());
 
             Entity entity = Entity.newBuilder(postKey)
+                    .set("id", username + "-" + System.currentTimeMillis())
                     .set("text", postText)
                     .set("user", username)
                     .set("timestamp", timestamp)
@@ -144,6 +150,7 @@ public class PostServlet extends HttpServlet {
         try{
             String tokenId = request.getHeader("Authorization");
             String username = request.getParameter("username");
+            String timestamp = request.getParameter("timestamp");
             LOG.fine("Attempt to create post for user " + username);
 
             //verifications
@@ -179,7 +186,61 @@ public class PostServlet extends HttpServlet {
                 return;
             }
 
+            StructuredQuery.OrderBy ascendingOrder = StructuredQuery.OrderBy.asc("timestamp");
+            Query<Entity> postQuery = Query.newEntityQueryBuilder()
+                    .setKind("Post")
+                    .setFilter(
+                            StructuredQuery.CompositeFilter.and(
+                                    StructuredQuery.PropertyFilter.hasAncestor(userKey),
+                                    StructuredQuery.PropertyFilter.gt("timestamp", timestamp)
+                            )
 
+                    )
+                    .addOrderBy(ascendingOrder)
+                    .setLimit(20)
+                    .build();
+
+            QueryResults<Entity> postResults = datastore.run(postQuery);
+
+            List<FeedData> posts = new ArrayList<>();
+            postResults.forEachRemaining(post->{
+
+                        String url = "";
+
+                        if(!post.getString("image").equals("")){
+
+                            BlobId blobId = BlobId.of( bucketName,
+                                    post.getString("user") + "-" + post.getString("image"));
+                            Blob blob = storage.get(blobId);
+                            url = blob.getMediaLink();
+                        }
+
+                        FeedData feedPost = new FeedData(
+                                post.getString("id"),
+                                post.getString("text"),
+                                post.getString("user"),
+                                url,
+                                post.getString("id").split("-")[1]
+                        );
+
+                        posts.add(feedPost);
+
+                    }
+            );
+
+            if(posts.isEmpty()){
+                response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
+                return;
+            }
+
+            // Convert the list of posts to JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(posts);
+
+            // Set the response content type and write the JSON string to the output stream
+            response.setContentType("application/json");
+            response.getWriter().write(json);
+            response.setStatus(HttpServletResponse.SC_OK);
 
         }catch (Exception e) {
             txn.rollback();
