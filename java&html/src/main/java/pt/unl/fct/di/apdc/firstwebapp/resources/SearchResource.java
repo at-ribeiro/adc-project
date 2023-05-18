c2aa231d11f4554a260577992a22f1c7bb469590
at-ribeiro <at.ribeiro@campus.fct.unl.pt>
2023-05-18 16:56:16 +0100

    Search testado quase 100%

--- java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/SearchResource.java
+++ java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/SearchResource.java
@@ -4,12 +4,15 @@ import com.google.cloud.datastore.*;
 import com.google.gson.Gson;
 import pt.unl.fct.di.apdc.firstwebapp.util.SearchUserData;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 @Path("/search")
 public class SearchResource {
@@ -18,6 +21,7 @@ public class SearchResource {
     private final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     private final Gson g = new Gson();
 
+    @GET
     @Path("/user")
     @Produces()
     public Response searchUser(@QueryParam("query") String query){
@@ -44,17 +48,23 @@ public class SearchResource {
             QueryResults<Entity> greaterThanOrEqualResults = datastore.run(queryGreaterThanOrEqual);
             QueryResults<Entity> lessThanResults = datastore.run(queryLessThan);
 
-            List<Entity> matchedUsers = new ArrayList<>();
+            List<SearchUserData> matchedUsers = new ArrayList<>();
             while (greaterThanOrEqualResults.hasNext() && lessThanResults.hasNext()) {
                 Entity user1 = greaterThanOrEqualResults.next();
                 Entity user2 = lessThanResults.next();
 
+                SearchUserData data1 = new SearchUserData(user1.getString("user_username"),
+                        user1.getString("user_fullname"));
+
+                SearchUserData data2 = new SearchUserData(user2.getString("user_username"),
+                        user2.getString("user_fullname"));
+                
                 // If the username matches in both queries, add it to the result list
                 if (user1.getString("user_username").startsWith(query)) {
-                    matchedUsers.add(user1);
+                    matchedUsers.add(data1);
                 }
-                if(user2.getString("user_username").startsWith(query)){
-                    matchedUsers.add(user2);
+                if(user2.getString("user_username").startsWith(query) && !data1.equals(data2)){
+                    matchedUsers.add(data2);
                 }
             }
             return Response.ok(g.toJson(matchedUsers)).build();
--- java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/SearchUserData.java
+++ java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/SearchUserData.java
@@ -12,5 +12,23 @@ public class SearchUserData {
         this.username = username;
         this.fullname = fullname;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof SearchUserData)) {
+            return false;
+        }
+        SearchUserData other = (SearchUserData) obj;
+        return this.username.equals(other.getUsername()) && this.fullname.equals(other.getFullname());
+    }
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 
 }
