78b434f23965b3f384255c312f7cd77f30b269e9
at-ribeiro <at.ribeiro@campus.fct.unl.pt>
2023-05-25 16:37:46 +0100

    a little progress

--- java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RegisterResource.java
+++ java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/RegisterResource.java
@@ -4,8 +4,12 @@ package pt.unl.fct.di.apdc.firstwebapp.resources;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.Timestamp;
 import com.google.cloud.datastore.*;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -22,6 +26,7 @@ import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
 import org.apache.commons.codec.binary.Base64;
 
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -135,50 +140,52 @@ public class RegisterResource {
         }
     }
 
-    private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory)
-            throws IOException {
 
-        InputStreamReader inputStream = new InputStreamReader(RegisterResource.class.getResourceAsStream("/client_secret.json"));
+    public static Message sendEmail(String fromEmailAddress,
+                                    String toEmailAddress)
+            throws MessagingException, IOException {
+        /* Load pre-authorized user credentials from the environment.
+           TODO(developer) - See https://developers.google.com/identity for
+            guides on implementing OAuth2 for your application.*/
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                .createScoped(GmailScopes.GMAIL_SEND);
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
 
-        System.out.println(inputStream.toString());
-
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, inputStream);
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, jsonFactory, clientSecrets, Set.of(GMAIL_SEND))
-                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
-                .setAccessType("offline")
+        // Create the gmail API client
+        Gmail service = new Gmail.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Gmail samples")
                 .build();
 
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
+        // Create the email content
+        String messageSubject = "noreply - Verify your FCT Connect email";
+        String bodyText = "lorem ipsum.";
 
-    public void sendMail(String subject, String message) throws Exception {
+        // Encode as MIME message
         Properties props = new Properties();
         Session session = Session.getDefaultInstance(props, null);
         MimeMessage email = new MimeMessage(session);
-        email.setFrom(new InternetAddress(TEST_EMAIL));
-        email.addRecipient(TO, new InternetAddress(TEST_EMAIL));
-        email.setSubject(subject);
-        email.setText(message);
+        email.setFrom(new InternetAddress(fromEmailAddress));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(toEmailAddress));
+        email.setSubject(messageSubject);
+        email.setText(bodyText);
 
+        // Encode and wrap the MIME message into a gmail message
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         email.writeTo(buffer);
         byte[] rawMessageBytes = buffer.toByteArray();
         String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
-        Message msg = new Message();
-        msg.setRaw(encodedEmail);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
 
         try {
-            NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-            GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
-            Gmail service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
-                    .setApplicationName("Test Mailer")
-                    .build();
-            msg = service.users().messages().send("me", msg).execute();
-            LOG.fine("Verification message id: " + msg.getId());
-            LOG.fine("Message: " + msg.toPrettyString());
+            // Create send message
+            message = service.users().messages().send("me", message).execute();
+            System.out.println("Message id: " + message.getId());
+            System.out.println(message.toPrettyString());
+            return message;
         } catch (GoogleJsonResponseException e) {
             GoogleJsonError error = e.getDetails();
             if (error.getCode() == 403) {
@@ -187,14 +194,16 @@ public class RegisterResource {
                 throw e;
             }
         }
+        return null;
     }
+}
 
     public void sendEmailVerification(String tokenId, String username) {
 
         String link = "https://fct-connect-estudasses.oa.r.appspot.com/rest/register/verification/" + username +"?token=" + tokenId;
 
         try{
-            sendMail("noreply - Verify your email",
+            sendMail("noreply - Verify your FCT Connect email",
                     "Thank you for registering! Please verify your account by clicking on the following link: \n" +
                     link);
         } catch (Exception e) {
