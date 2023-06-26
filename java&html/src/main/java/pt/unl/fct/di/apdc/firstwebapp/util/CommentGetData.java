9e612230b6679165e2552801bc75bfbe465c83ac
at-ribeiro <at.ribeiro@campus.fct.unl.pt>
2023-06-26 23:55:40 +0100

    beta completa (bug fixed)

--- java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/CommentGetData.java
+++ java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/CommentGetData.java
@@ -5,13 +5,15 @@ public class CommentGetData {
     private String user;
     private String text;
     private long timestamp;
+    private String profilePic;
 
     public CommentGetData(){}
 
-    public CommentGetData(String user, String text, long timestamp){
+    public CommentGetData(String user, String text, long timestamp, String profilePic) {
         this.user = user;
         this.text = text;
         this.timestamp = timestamp;
+        this.profilePic = profilePic;
     }
 
 }
