c857d2f9f32a95578414b0dba01abe0f401a5dd6
at-ribeiro <at.ribeiro@campus.fct.unl.pt>
2023-06-27 15:25:31 +0100

    pwd change and delete user done

--- java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/CPData.java
+++ java&html/src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/CPData.java
@@ -2,24 +2,16 @@ package pt.unl.fct.di.apdc.firstwebapp.util;
 
 public class CPData {
 
-    private String username;
-    private String tokenId;
     private String newPassword;
     private String oldPassword;
     private String passwordV;
 
     public CPData(){}
 
-    public CPData(String username, String tokenId, String password, String oldPassword, String passwordV){
-        this.username = username;
-        this.tokenId = tokenId;
+    public CPData(String password, String oldPassword, String passwordV){
         this.newPassword = password;
         this.oldPassword = oldPassword;
         this.passwordV = passwordV;
-    }
-
-    public String getUsername(){
-        return username;
     }
 
     public String getNewPassword() {
@@ -35,10 +27,8 @@ public class CPData {
     }
 
     public boolean valid(){
-       return passwordV.equals(oldPassword);
+       return passwordV.equals(newPassword);
     }
 
-    public String getTokenId() {
-        return tokenId;
-    }
+
 }
